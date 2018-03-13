db9a49992eef94d7169477d1558032e6088d1b92
Woosuk Lee <wlee10@stuy.edu>
2018-03-13 14:06:18 -0400

    hi

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -4,9 +4,27 @@
   APCS pd08
   Lab01 -- What does the data say?
   2018-03-14
- */
-import time;
+*/
+
 
 public class QuickSortTester{
-    
+
+    public static void main( String[] args ){
+
+	for ( int n = 1000; n < 10000; n += 100 ) {
+	    QuickSort data = new QuickSort();
+	   
+	    int d[] = data.buildArray(n,n);
+	    data.shuffle(d);
+	    
+	    double a = System.currentTimeMillis();	     
+
+	    QuickSort.qsort(d);
+
+	    double b = System.currentTimeMillis();
+
+	    System.out.println((b-a));
+	    
+       	}	
+    }
 }
