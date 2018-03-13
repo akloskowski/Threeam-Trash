139b927d371c0d86d113242cc2ab83237c7a1291
Woosuk Lee <wlee10@stuy.edu>
2018-03-13 13:46:02 -0400

    tester added

---
+++ QuickSortTester.java
@@ -0,0 +1,12 @@
+/*
+  Team Threeam Trash
+  Adrian Kloskowski, Ryan Kim, Woosuk Lee
+  APCS pd08
+  Lab01 -- What does the data say?
+  2018-03-14
+ */
+import time;
+
+public class QuickSortTester{
+    
+}
